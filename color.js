b03df297c2b120c93a076dd37fc39131656558f4
worthyjyj <108921165+worthyjyj@users.noreply.github.com>
2022-08-22 12:26:57 +0900

    자바스크립트 파일로 쪼개기_유지보수 효율성 높이기

---
+++ color.js
@@ -0,0 +1,27 @@
+   // 야간모드 버튼 클릭했을 때 자바스크립트를 메소드화 (함수화)
+   function nightDayHandler(self){
+    var target = document.querySelector('body');
+if(self.value === 'night'){
+target.style.backgroundColor='black';
+target.style.color='White';
+self.value='day';
+var alist = document.querySelectorAll('a');
+var i = 0;
+while(i < alist.length){
+    alist[i].style.color = 'powderblue';
+    i=i+1
+}
+}
+
+else{
+target.style.backgroundColor='White';
+target.style.color='black';
+self.value='night';
+var alist = document.querySelectorAll('a');
+var i = 0;
+while(i < alist.length){
+    alist[i].style.color = 'blue';
+    i=i+1
+}
+}
+}
